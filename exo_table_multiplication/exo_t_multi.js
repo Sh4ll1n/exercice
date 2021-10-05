689b6298dc9edea364c6c6e3f13dbdbf7146c9f7
Julien Legros <julien.legros91@gmail.com>
2021-10-05 16:55:11 +0200

    envoi exo

--- exo_table_multiplication/exo_t_multi.js
+++ exo_table_multiplication/exo_t_multi.js
@@ -1,7 +1,10 @@
 // Ecrire une fonction javascript retournant la table de multiplication d’une valeur donnée en paramètre. 
 // Avant l'appel à la fonction la valeur doit être saisie par l'intermediaire d'une fenêtre de dialogue.
 
-var nbre = prompt("Saisissez le nombre")
+
+
+
+var nombre = prompt("Saisissez le nombre")
 
 function tablem(nbre){
     for(var i = 1; i <= 10; i++){
@@ -9,10 +12,27 @@ function tablem(nbre){
     }
 }
 tablem(nbre);
+
+
+
 // var nbre = prompt("Saississez le nombre")
 // var tablem = [1,2,3,4,5,6,7,8,9,10]
 // Je veux faire nbre*tablem (donc chaque valeur a l'interieur de mon tableau)
+function tablem(nbre){
+    var resultat = 0;
+    for(var i = 1; i <= 10; i++){
+        // resultat += nbre,"*",i,"=",nbre*i;
+        resultat += nbre+" * "+i+" = ",+nbre*i+"\n";
+        // \n saut de lignes
+        // += une sorte d'incrementation 
+        // concatené avec des virgules font qu'elles deviennent des virgules
+        // console.log("La valeur "+ nbre +" multiplié par "+ i +" est égale à "+nbre * i);
+    }
+    // console.log(resultat);
+    return resultat;
+}
 
+console.log(tablem(nombre));
 
     
 
