4fcd4d6f75ccd6eb94568e1d967c06c20093b84a
Julien L <julien.legros91@gmail.com>
2021-10-05 23:26:35 +0200

    ajout commentaire

--- exo_one_to_three/exo_one_to_three.js
+++ exo_one_to_three/exo_one_to_three.js
@@ -12,4 +12,4 @@ for(var i = 0; i <= 10; i++){
         console.log("Try again");
 }
 
-// J'arrive bien que 1 2 et 3 sont les bonnes réponses a la question. Mais je n'arrive pas a ce que cela se relance automatiquement. 
+// J'arrive bien que 1 2 et 3 sont les bonnes réponses a la question. Mais je n'arrive pas a ce que cela se relance automatiquement. Pareil je ne sais pas quoi mettre comme condition. S
