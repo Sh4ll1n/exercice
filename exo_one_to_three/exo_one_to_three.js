d2d205a92089db0742fabced1e08e442abd5b054
Julien Legros <julien.legros91@gmail.com>
2021-10-06 17:03:12 +0200

    maj

--- exo_one_to_three/exo_one_to_three.js
+++ exo_one_to_three/exo_one_to_three.js
@@ -1,15 +1,16 @@
 // Ecrire un script qui demande à l’utilisateur un nombre compris entre 1 et 3 jusqu’à ce
 // que la réponse convienne.
 
-var nbre = prompt("Veuillez saisir un nombre entre et trois");
+var nbre = prompt("Veuillez saisir un nombre entre un et trois");
 console.log(nbre);
-for(var i = 0; i <= 10; i++){
+for(var i = 0; i <= nbre; i++){
     if (nbre == 1 || nbre == 2 || nbre == 3){
-        console.log("Bonne reponse");
-        break;
+        console.log("C'est bon tu peux passer");
+        break; //Pas sur de son fonctionnement; au final bien placer !!
     }
     else 
+        nbre=prompt("Veuillez saisir un nombre entre un et trois");
         console.log("Try again");
 }
 
-// J'arrive bien que 1 2 et 3 sont les bonnes réponses a la question. Mais je n'arrive pas a ce que cela se relance automatiquement. Pareil je ne sais pas quoi mettre comme condition. S
+// J'arrive bien que 1 2 et 3 sont les bonnes réponses a la question. Mais je n'arrive pas a ce que cela se relance automatiquement. Pareil je ne sais pas quoi mettre comme condition. 
